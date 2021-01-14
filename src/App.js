6578cd01d373a9089e7bfe37e8bac610cebb33e3
Bryan Lown <info@lp-digital.co.uk>
2021-01-14 18:30:36 +0000

    First commmit.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Main from "./components/MainComponent";
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from "./components/MenuComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+
+  render() {
+    return (
+        <div className="App">
+            <Main />
         </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+        );
+    }
 }
 
 export default App;
